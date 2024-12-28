524829b4330724f676774bbf88a250353edd9540
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-12-28 21:01:14 +0800

    added "Remember me" to login page

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -2,8 +2,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:normal_list/app/core/utils/validators.dart';
+import 'package:normal_list/app/core/widgets/button.dart';
+import 'package:normal_list/app/core/widgets/text_form_field.dart';
 import 'package:normal_list/app/router.dart';
 import 'package:normal_list/features/auth/data/auth_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -19,7 +22,46 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController passwordController = TextEditingController();
 
   final AuthService authService = AuthService();
+  
+  bool rememberMe = false;
 
+  @override
+  void initState() {
+    super.initState();
+    _loadRememberedCredentials();
+  }
+
+  // Load remembered credentials
+  Future<void> _loadRememberedCredentials() async {
+    final prefs = await SharedPreferences.getInstance();
+    final email = prefs.getString('email');
+    final password = prefs.getString('password');
+    final isRemembered = prefs.getBool('rememberMe') ?? false;
+
+    if(isRemembered && email != null && password != null){
+      emailController.text = email;
+      passwordController.text = password;
+      setState(() {
+        rememberMe = isRemembered;
+      });
+    }
+  }
+
+  // Save credentials
+  Future<void> _saveCredentials() async {
+    final prefs = await SharedPreferences.getInstance();
+    if(rememberMe){
+      prefs.setString('email', emailController.text);
+      prefs.setString('password', passwordController.text);
+      prefs.setBool('rememberMe', true);
+    } else {
+      prefs.remove('email');
+      prefs.remove('password');
+      prefs.setBool('rememberMe', false);
+    }
+  }
+
+  // Handle login
   void handleLogin(BuildContext ctx) async {
 
     if(!_formKey.currentState!.validate()) return;
@@ -34,6 +76,8 @@ class _LoginScreenState extends State<LoginScreen> {
         SnackBar(content: Text('Login failed!'))
       );
     } else {
+      await _saveCredentials();
+      if(!ctx.mounted) return;
       ScaffoldMessenger.of(ctx).showSnackBar(
         SnackBar(content: Text('Welcome to your list!'))
       );
@@ -51,26 +95,43 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextFormField(
+                AppTextFormField(
                   controller: emailController,
-                  decoration:  InputDecoration(labelText: 'Email'),
+                  label: 'Email',
                   keyboardType: TextInputType.emailAddress,
                   validator: FormValidators.validateEmail,
                 ),
-                TextFormField(
+                SizedBox(height: 20),
+                AppTextFormField(
                   controller: passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
+                  label: 'Password',
                   validator: FormValidators.validatePassword,
                   obscureText: true,
                 ),
                 SizedBox(height: 20),
-                ElevatedButton(
-                  style: ButtonStyle(),
-                  onPressed: () {
-                    handleLogin(context);
-                  },
-                  child: Text('Login')
-                )
+                Row(
+                  children: [
+                    Checkbox(
+                      value: rememberMe, onChanged: (bool? value) {
+                        setState(() {
+                          rememberMe = value ?? false;
+                        });
+                      }
+                    ),
+                    Text('Remember Me')
+                  ],
+                ),
+                SizedBox(height: 40),
+                SizedBox(
+                  width: 200, // Set the width
+                  height: 40, // Set the height
+                  child: AppButton(
+                    onPressed: () {
+                      handleLogin(context);
+                    },
+                    label: 'Login',
+                  ),
+                ),
               ],
             ),
           )
