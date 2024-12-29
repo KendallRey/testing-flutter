2e6230e476acd14afc46c9a3ebffeeda143cd969
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-12-30 01:54:19 +0800

    added AppProvider, with secret

---
+++ lib/app/core/services/app_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppProvider with ChangeNotifier {
+  String _secret = '';
+
+  String get secret => _secret;
+
+  void setSecret(String value){
+    _secret = value;
+    notifyListeners();
+  }
+
+}
