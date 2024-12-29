aef1a5da898818143bb4da4e95f66b313aa09529
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-12-29 19:41:21 +0800

    added base model

---
+++ lib/app/core/model/model.dart
@@ -0,0 +1,13 @@
+
+class Model {
+
+  static final String idKey = 'id';
+  static final String createdAtKey = 'createdAt';
+  static final String updatedAtKey = 'updatedAt';
+
+  final String id;
+
+  Model({
+    required this.id,
+  });
+}
