38223f112c4ab5cc48c2f8bb3b108b64ae12aa0e
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-12-29 19:43:04 +0800

    added validateRequiredString to FormValidator class

--- lib/app/core/utils/validators.dart
+++ lib/app/core/utils/validators.dart
@@ -14,4 +14,11 @@ class FormValidators {
     }
     return null;
   }
+
+  static String? validateRequiredString(String? value) {
+    if(value == null || value.isEmpty){
+      return 'This field is required';
+    }
+    return null;
+  }
 }
