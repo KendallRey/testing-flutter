0de5f422053fde8abd146e307e05fee8a855f128
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-12-30 01:53:31 +0800

    added PreferenceKeys constants

---
+++ lib/app/core/constants/preference_keys.dart
@@ -0,0 +1,8 @@
+
+class PreferenceKeys {
+  static const String prefix = '@my-normal-list';
+  static const String email = '$prefix-email';
+  static const String password = '$prefix-password';
+  static const String rememberMe = '$prefix-remember-me';
+  static const String secret = '$prefix-secret';
+}
