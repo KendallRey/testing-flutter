77e493f35730ef40a6f685a4469584f2e42ec1aa
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-01-01 01:00:16 +0800

    added initial item view page

--- lib/features/list/presentation/screens/view_list_item_screen.dart
+++ lib/features/list/presentation/screens/view_list_item_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:normal_list/app/core/widgets/button.dart';
 import 'package:normal_list/features/list/data/list_service.dart';
 
 class ViewListItemScreen extends StatefulWidget {
@@ -31,9 +32,62 @@ class _ViewListItemScreenState extends State<ViewListItemScreen> {
         if(!snapshot.hasData || snapshot.data == null){
           return Center(child: Text('No data found'));
         }
+        final item = snapshot.data!;
         return Scaffold(
           appBar: AppBar(
-            title: Text(snapshot.data!.code),
+            title: Text(item.code),
+          ),
+          body: Padding(
+              padding: EdgeInsets.all(24),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        Card(
+                          child: SizedBox(
+                          height: 320.0,
+                          width: 320.0,
+                          child: item.image != null ?
+                            Image(
+                                image: FileImage(item.image!)
+                            ) :
+                            Center(
+                              child: Text('No image.'),
+                            )
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(item.code,
+                          style: Theme.of(context).textTheme.titleLarge,
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 20),
+                        Text(item.title,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    )
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    spacing: 24,
+                    children: [
+                      AppButton(onPressed: (){},
+                        label: 'Delete',
+                        width: 150.0,
+                        disabled: true,
+                      ),
+                      AppButton(onPressed: (){},
+                        label: 'Edit',
+                        width: 150.0,
+                        disabled: true,
+                      ),
+                    ],
+                  )
+                ],
+              ),
           ),
         );
       },
