8aadcbd722fa16ddc3599209c7706d83bbc0de62
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-01-01 22:38:46 +0800

    future-fix, add alert dialog confirm on close app

--- lib/app/observers/app_life_observer.dart
+++ lib/app/observers/app_life_observer.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:normal_list/app/core/services/temp_file_manager.dart';
 
 class AppLifecycleObserver extends StatefulWidget {
@@ -46,8 +47,58 @@ class _AppLifecycleObserverState extends State<AppLifecycleObserver> with Widget
     }
   }
 
+  // its not working, maybe in the future,
+  // see more: https://github.com/flutter/flutter/issues/138737
+  Future<bool?> _showExitAppDialog() async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Exit App'),
+            content: Text('Are you sure you want to close the application?'),
+            actions: [
+              TextButton(
+                onPressed: () => context.pop(false), // Cancel
+                child: Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () => context.pop(true),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ), // Confirm
+                child: Text('Exit'),
+              ),
+            ],
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return widget.child;
+    return PopScope(
+      canPop: true,
+      onPopInvokedWithResult: (bool didPop, Object? result) async {
+        if(kDebugMode){
+          print('Pop invoked!');
+        }
+        if (didPop) {
+          return;
+        }
+        if(kDebugMode){
+          print('Pop invoked!: 2');
+        }
+        final bool shouldPop = await _showExitAppDialog() ?? false;
+        if (shouldPop) {
+          // Since this is the root route, quit the app where possible by
+          // invoking the SystemNavigator. If this wasn't the root route,
+          // then Navigator.maybePop could be used instead.
+          if(context.mounted){
+            context.pop();
+          }
+        }
+      },
+      child: widget.child
+    );
   }
 }
