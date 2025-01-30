be01e17b7968682f7d168f64e3852edc17a818de
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-01-30 23:17:21 +0800

    Added scroll up / down Fab to list page

--- lib/features/list/presentation/screens/list_screen.dart
+++ lib/features/list/presentation/screens/list_screen.dart
@@ -7,6 +7,8 @@ import 'package:normal_list/features/list/data/list_service.dart';
 class ListScreen extends StatelessWidget {
   ListScreen({super.key});
 
+  final ScrollController _scrollController = ScrollController();
+
   final User? user = FirebaseAuth.instance.currentUser;
   final ListService _listService = ListService();
 
@@ -15,6 +17,22 @@ class ListScreen extends StatelessWidget {
       ctx.pushNamed(AppRoutes.viewListItemName,
           pathParameters: {AppRoutes.id: id});
     }
+  }
+
+  void _scrollToBottom() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeOut,
+    );
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(
+      _scrollController.position.minScrollExtent,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeOut,
+    );
   }
 
   @override
@@ -29,69 +47,93 @@ class ListScreen extends StatelessWidget {
             return Center(child: Text('No data found'));
           }
           final items = snapshot.data!;
-          return GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            ),
-            padding: EdgeInsets.all(8.0),
-            itemCount: items.length,
-            itemBuilder: (ctx, index) {
-              var item = items[index];
-              ImageProvider<Object> imageDisplay = item.image != null
-                  ? FileImage(item.image!)
-                  : AssetImage('assets/sample.png');
-              return Card(
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(16),
-                  onTap: () {
-                    handleClickItem(context, item.id);
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                        image: imageDisplay,
-                        fit: BoxFit.cover,
-                      )),
-                      child: Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: [
-                          Positioned(
-                              bottom: 0,
-                              left: 0,
-                              right: 0,
-                              child: Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).primaryColorDark,
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      item.code,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .labelLarge,
+          return Stack(
+            children: [
+              GridView.builder(
+                controller: _scrollController,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                padding: EdgeInsets.all(8.0),
+                itemCount: items.length,
+                itemBuilder: (ctx, index) {
+                  var item = items[index];
+                  ImageProvider<Object> imageDisplay = item.image != null
+                      ? FileImage(item.image!)
+                      : AssetImage('assets/sample.png');
+                  return Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(16),
+                      onTap: () {
+                        handleClickItem(context, item.id);
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                            image: imageDisplay,
+                            fit: BoxFit.cover,
+                          )),
+                          child: Stack(
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                              Positioned(
+                                  bottom: 0,
+                                  left: 0,
+                                  right: 0,
+                                  child: Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      color: Theme.of(context).primaryColorDark,
                                     ),
-                                    Text(
-                                      overflow: TextOverflow.ellipsis,
-                                      item.title,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .labelMedium,
-                                    )
-                                  ],
-                                ),
-                              ))
-                        ],
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          item.code,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelLarge,
+                                        ),
+                                        Text(
+                                          overflow: TextOverflow.ellipsis,
+                                          item.title,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelMedium,
+                                        )
+                                      ],
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              );
-            },
+                  );
+                },
+              ),
+              Positioned(
+                  left: 20,
+                  bottom: 20,
+                  child: Opacity(
+                    opacity: 0.3,
+                    child: Column(
+                      children: [
+                        FloatingActionButton.small(
+                          onPressed: _scrollToTop,
+                          child: Icon(Icons.arrow_upward),
+                        ),
+                        FloatingActionButton.small(
+                          onPressed: _scrollToBottom,
+                          child: Icon(Icons.arrow_downward),
+                        )
+                      ],
+                    ),
+                  ))
+            ],
           );
         });
   }
